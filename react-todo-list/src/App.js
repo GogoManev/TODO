8578ff0fafa8fb52aa0e7898f4d0a2e07dd33a8d
GogoManev <georgy.manev@gmail.com>
2025-03-15 13:12:02 +0200

    todo

---
+++ react-todo-list/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <a1> TODO (WIP) </a1> <br></br>
+    <button> Click me </button>
+    <img src={logo} className="ReactLogo" alt="logo" />
+    </>
+  );
+}
+
+export default App;
